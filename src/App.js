d1639a51c530e7e8ce4a1c4844722a819476a5fd
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-04-25 03:50:40 +0200

    hero section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Hero />
     </Router>
   );
 }
