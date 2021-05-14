24487097241c62249a29b438e2281a2f16289b94
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-05-14 23:04:37 +0200

    product details added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Hero from "./components/Hero";
+import { productData } from "./components/Products/data";
+import Products from "./components/Products/Products";
 
 function App() {
   return (
     <Router>
       <Hero />
+      <Products heading='Choose your favorite' data={productData} />
     </Router>
   );
 }
---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from "../../images/product-1.jpeg";
+import product2 from "../../images/product-2.jpeg";
+import product3 from "../../images/product-3.jpeg";
+
+export const productData = [
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Supreme Pizza",
+    desc: "Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product2,
+    alt: "Pizza",
+    name: "Hawaiian Paradise",
+    desc: " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "$16.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product3,
+    alt: "Pizza",
+    name: "Veggie Overload",
+    desc: " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "$14.99",
+    button: "Add to Cart",
+  },
+];
